53ad6433de597af3afb8acaffd804b4b146ae3bd
melegao <melegao@gmail.com>
2023-01-20 18:43:30 -0300

    fix: Installed cors

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors")
 
 const app = express();
 const port = 3333;
+
+app.use(cors())
 
 routes(app);
 
