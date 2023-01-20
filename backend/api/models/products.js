26cd86c0d4be0fc450fa4a40d53883cce490d70e
melegao <melegao@gmail.com>
2023-01-20 14:37:27 -0300

    feat: Created Products model and migration

--- backend/api/models/products.js
+++ backend/api/models/products.js
@@ -3,25 +3,25 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Users extends Model {
+  class Products extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Users.hasMany(models.Products, {
+      Products.belongsTo(models.Users, {
         foreignKey: 'ownerId'
       })
     }
   }
-  Users.init({
+  Products.init({
     name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    description: DataTypes.STRING,
+    price: DataTypes.DECIMAL
   }, {
     sequelize,
-    modelName: 'Users',
+    modelName: 'Products',
   });
-  return Users;
+  return Products;
 };
