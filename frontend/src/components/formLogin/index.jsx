e74cce084e5dfd78a92d9a179ce4337ff6fb36e0
melegao <melegao@gmail.com>
2023-01-21 13:47:35 -0300

    feat: Developed default input and login form.

--- frontend/src/components/formLogin/index.jsx
+++ frontend/src/components/formLogin/index.jsx
@@ -1,17 +1,29 @@
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
 import api from "../../services/api";
 import { toast } from "react-toastify";
 import { FormLoginContainer } from "./styles";
+import { InputBase } from "../input";
 
 function FormLogin() {
-  const { register, handleSubmit } = useForm();
+  const formSchema = yup.object().shape({
+    email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+    password: yup.string().required("Campo obrigatório"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(formSchema) });
 
   const onSubmit = (data) => {
     console.log(data);
-    api
-      .post("/login", data)
-      .then((res) => handleSuccess(res.data))
-      .catch((err) => handleError());
+    // api
+    //   .post("/login", data)
+    //   .then((res) => handleSuccess(res.data))
+    //   .catch((err) => handleError());
   };
 
   const handleSuccess = (data) => {
@@ -24,10 +36,26 @@ function FormLogin() {
     toast.error("Verifique e-mail ou senha");
   };
 
+
   return (
     <FormLoginContainer onSubmit={handleSubmit(onSubmit)}>
-      <input placeholder="E-mail" {...register("email")} />
-      <input placeholder="Password" type="password" {...register("password")} />
+      <InputBase
+        type="text"
+        placeholder="Digite seu e-mail"
+        name="email"
+        register={register}
+        label="E-mail"
+        error={errors?.email?.message}
+      />
+      <InputBase
+        type="password"
+        register={register}
+        placeholder="Digitar senha"
+        name="password"
+        label="Senha"
+        error={errors?.password?.message}
+      />
+
       <button type="submit">Login</button>
     </FormLoginContainer>
   );
--- frontend/src/components/input/index.jsx
+++ frontend/src/components/input/index.jsx
@@ -1,9 +1,44 @@
+import { ContainerInput, DefaultInput } from "./styles";
 
+export const InputBase = ({
+  placeholder,
+  type,
+  name,
+  register,
+  children,
+  label,
+  error,
+  width,
+  ...rest
+}) => {
+  return (
+    <ContainerInput>
+      <div className="label--input">
+        <label>
+          <p>{label}</p>
+        </label>
+      </div>
+      <div>
+        <DefaultInput
+          placeholder={placeholder}
+          type={type}
+          error={error}
+          width={width}
+          name={name}
+          {...(register !== undefined && register(name))}
+          {...rest}
+        >
+          {children}
+        </DefaultInput>
 
-export const InputBase = () => {
-
-
-    return (
-        <></>
-    )
-}
+        {error !== undefined && (
+          <div className="label--error">
+            <label>
+              <p>{error}</p>
+            </label>
+          </div>
+        )}
+      </div>
+    </ContainerInput>
+  );
+};
